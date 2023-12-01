44127efae22b07efa0414e2fe509383e7e34ae95
falahrafif <frthegamer95@gmail.com>
2023-12-01 14:34:33 +0700

    Create Controller

--- 3 Show Data/belajar-laravel-9/app/Http/Controllers/PostController.php
+++ 3 Show Data/belajar-laravel-9/app/Http/Controllers/PostController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post; # menambah model 
 
 class PostController extends Controller
 {
-    //
+    #menambahkan method baru
+    public function index()
+    {
+        //get posts
+        $posts = Post::latest()->paginate(5);
+
+        //render view with posts data
+        return view('posts.index', compact('posts'));
+    }
 }
